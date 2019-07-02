7e14d7277e7147bf9d878d3082367facdb800a7c
linaksel <linrosaksel@gmail.com>
2019-07-02 15:54:25 +0300

    a-kohdan muutokset

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -1,12 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Header = (props) => {
+    return(
+        <div>
+            <h1>{props.course}</h1>
+        </div>
+    )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Part = (props) => {
+    return (
+        <p>{props.name} {props.exes}</p>
+    )
+}
+
+const Content = (props) => {
+    return (
+        <div>
+            <Part name={props.name1} exes={props.exes1}/>
+            <Part name={props.name2} exes={props.exes1}/>
+            <Part name={props.name3} exes={props.exes3}/>
+        </div>
+    )
+}
+
+const Total = (props) => {
+    return (
+        <div>
+            <p>Number of exercises {props.exes1 + props.exes2 + props.exes3}</p>
+        </div>
+    )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content name1={part1} name2={part2} name3={part3} exes1={exercises1}
+      exes2={exercises2} exes3={exercises3} />
+      <Total exes1={exercises1} exes2={exercises2} exes3={exercises3} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
