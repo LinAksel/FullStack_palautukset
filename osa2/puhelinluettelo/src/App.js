d22988bfb6db1a7953d6abd004eaf9d120ca585c
Aksel Linros <aksel.linros@nitor.com>
2023-04-11 23:50:30 +0300

    task 2.9: add filter

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -10,10 +10,14 @@ const Person = ({ person }) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "0401233567" },
+    { name: "Arto Hellas", number: "040-123456" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
   ])
   const [newName, setNewName] = useState("")
   const [newNumber, setNewNumber] = useState("")
+  const [filter, setFilter] = useState("")
 
   const handleSubmit = (event) => {
     event.preventDefault()
@@ -32,10 +36,18 @@ const App = () => {
 
   const handleNameChange = (event) => setNewName(event.target.value)
   const handleNumberChange = (event) => setNewNumber(event.target.value)
+  const handleFilterChange = (event) => setFilter(event.target.value)
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <form>
+        <div>
+          filter shown with:{" "}
+          <input value={filter} onChange={handleFilterChange} />
+        </div>
+      </form>
+      <h2>Add new</h2>
       <form>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -51,9 +63,13 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map((person) => (
-          <Person key={person.name} person={person} />
-        ))}
+        {persons
+          .filter((person) =>
+            person.name.toLowerCase().includes(filter.toLowerCase())
+          )
+          .map((person) => (
+            <Person key={person.name} person={person} />
+          ))}
       </div>
     </div>
   )
