b3e46b0bba4771e0b5daec249d8d36c218b61a32
Aksel Linros <aksel.linros@nitor.com>
2023-04-11 23:05:57 +0300

    task 2.6: initial phonebook with names

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,45 @@
+import { useState } from "react"
+
+const Person = ({ name }) => {
+  return <div>{name}</div>
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }])
+  const [newName, setNewName] = useState("")
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+    }
+    setPersons(persons.concat(personObject))
+    setNewName("")
+  }
+
+  const handleChange = (event) => setNewName(event.target.value)
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={handleChange} />
+        </div>
+        <div>
+          <button type="submit" onClick={handleSubmit}>
+            add
+          </button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map((person) => (
+          <Person key={person.name} name={person.name} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default App
